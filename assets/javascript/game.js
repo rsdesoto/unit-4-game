02842f6535574fa91b3a937f98e80bf5b1e40528
Ry <ry.e.desoto@gmail.com>
2018-11-13 01:14:34 -0600

    logic done, aesthetics started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,106 +1,120 @@
-var crystalArray = ["blue", "yellow", "red", "green"];
+$(document).ready(function() {
+    //////////////////////////////////////////////////////////////////
+    // VARIABLES /////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
 
-var wins;
-var losses;
-var gamesPlayed;
+    // initialize the array of crystals. If more crystals are added, update this array accordingly
+    var crystalArray = ["blue", "yellow", "red", "green"];
 
-var currentScore;
-var currentTarget;
+    // initialize variables that carry over round to round
+    var wins;
+    var losses;
+    var gamesPlayed;
 
-// set the random values for the crystals
-function setCrystalValue() {
-    for (i = 0; i < crystalArray.length; i++) {
-        // create a random number between 1 - 12
-        crysVal = Math.floor(Math.random() * 12 + 1);
+    // initialize variables for each round
+    var currentScore;
+    var currentTarget;
 
-        // get the crystal ID
-        crystal = $("#" + crystalArray[i]);
-        console.log(crystal.attr("class"));
+    //////////////////////////////////////////////////////////////////
+    // FUNCTIONS /////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
 
-        // assign the random number to the crystal as data-value
-        crystal.attr("data-value", crysVal);
-        console.log(crystal.attr("data-value"));
+    // set the random values for the crystals
+    function setCrystalValue() {
+        for (i = 0; i < crystalArray.length; i++) {
+            // create a random number between 1 - 12
+            crysVal = Math.floor(Math.random() * 12 + 1);
+
+            // get the crystal ID
+            crystal = $("#" + crystalArray[i]);
+            console.log(crystal.attr("class"));
+
+            // assign the random number to the crystal as data-value
+            crystal.attr("data-value", crysVal);
+            console.log(crystal.attr("data-value"));
+        }
     }
-}
 
-setCrystalValue();
-
-// set the random number the user is trying to get;
-function setGuessNumber() {
-    currentTarget = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-    console.log(currentTarget);
-}
-
-setGuessNumber();
-
-// clear out current score;
-function clearScore() {
-    currentScore = 0;
-}
-
-clearScore();
-
-// create a function that calls all the reset variables
-function setRound() {
-    setCrystalValue();
-    setGuessNumber();
-    clearScore();
-    $("#roundendtext").hide();
-    $("#resetbutton").hide();
-}
-
-// create a function that initializes the game upon page refresh
-function setGame() {
-    wins = 0;
-    losses = 0;
-    gamesPlayed = 0;
-    setRound();
-}
-
-setGame();
-
-// create a function that prints wins, losses, games played, and the target score;
-function printVals() {
-    $("#wins").html(wins);
-    $("#losses").html(losses);
-    $("#gamesplayed").html(gamesPlayed);
-    $("#targetnum").html(currentTarget);
-    $("#playerscore").html(currentScore);
-}
-
-// check for win/loss
-function checkGameEnd() {
-    if (currentScore < currentTarget) {
-        // nothing: keep playing
-    } else if (currentScore === currentTarget) {
-        wins++;
-        $("#roundendtext").show();
-        $("#roundendtext").html("Congratulations! You win!");
-        $("#resetbutton").show();
-    } else if (currentScore > currentTarget) {
-        losses++;
-        $("#roundendtext").show();
-        $("#roundendtext").html("Too bad :c You lost");
-        $("#resetbutton").show();
+    // set the random number the user is trying to get;
+    function setGuessNumber() {
+        currentTarget = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+        console.log(currentTarget);
     }
-}
 
-// on click: get the data value of the crystal clicked on, add to user score, and check for win/lose condition
-$(".crystal").on("click", function() {
-    // only run this as long as currentScore from user is below the current Target number
-    if (currentScore < currentTarget) {
-        var dataval = $(event.target).attr("data-value");
-        console.log(dataval);
-        currentScore += parseInt(dataval);
-        console.log(currentScore);
+    // clear out current score;
+    function clearScore() {
+        currentScore = 0;
+    }
+
+    // create a function that prints wins, losses, games played, and the target score;
+    function printVals() {
+        $("#wins").html(wins);
+        $("#losses").html(losses);
+        $("#gamesplayed").html(gamesPlayed);
+        $("#targetnum").html(currentTarget);
         $("#playerscore").html(currentScore);
-
-        checkGameEnd();
     }
-});
 
-// on click: reset the game
-$("#resetbutton").on("click", function() {
-    setRound();
-    printVals();
+    // create a function that calls all the reset variables
+    function setRound() {
+        setCrystalValue();
+        setGuessNumber();
+        clearScore();
+        printVals();
+        $("#gameendtext").hide();
+        // $("#resetbutton").hide();
+    }
+
+    // create a function that initializes the game upon page refresh
+    function setGame() {
+        wins = 0;
+        losses = 0;
+        gamesPlayed = 0;
+        setRound();
+    }
+
+    // check for win/loss
+    function checkGameEnd() {
+        if (currentScore === currentTarget) {
+            wins++;
+            gamesPlayed++;
+            $("#gameendtext").show();
+            $("#roundendtext").html("Congratulations! You win!");
+            // $("#resetbutton").show();
+            printVals();
+        } else if (currentScore > currentTarget) {
+            losses++;
+            gamesPlayed++;
+            $("#gameendtext").show();
+            $("#roundendtext").html("Too bad :c You lost");
+            // $("#resetbutton").show();
+            printVals();
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////
+    // GAME //////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
+
+    // set the initial variables and prep the game for being played
+    setGame();
+
+    // when user clicks on a crystal, grab the value, add it to the current score, and check to see if the game should end
+    $(".crystal").on("click", function() {
+        // only run this as long as currentScore from user is below the current Target number
+        if (currentScore < currentTarget) {
+            var dataval = $(event.target).attr("data-value");
+
+            currentScore += parseInt(dataval);
+
+            $("#playerscore").html(currentScore);
+
+            checkGameEnd();
+        }
+    });
+
+    // on clicking on the rest button: reset the game
+    $("#resetbutton").on("click", function() {
+        setRound();
+    });
 });
